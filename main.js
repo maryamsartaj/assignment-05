0a2302b2f1c18fffc0189a4c36a8526b0c23d360
maryamsartaj <168002522+maryamsartaj@users.noreply.github.com>
2024-04-27 01:25:23 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var qoute = "A person who never made a mistake never tried anything new";
+var famouse_person = "Albert Einstein";
+var message = "".concat(famouse_person, " once said, ").concat(qoute);
+console.log(message);
